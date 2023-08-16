76aa7db44daec6e9f3c8dcc11c5cc816cc889f71
schiho47 <84003532+schiho47@users.noreply.github.com>
2023-08-16 18:54:55 +0800

    20230816 修正點擊效果ing

--- src/App.js
+++ src/App.js
@@ -1,122 +1,151 @@
-import { useState } from 'react';
-import styles from'./App.module.scss';
-import NumberItem from './components/NumberItem/NumberItem';
-import Result from './components/Result/Result';
+import { useState } from "react";
+import styles from "./App.module.scss";
+import NumberItem from "./components/NumberItem/NumberItem";
+import Result from "./components/Result/Result";
 
-const numberItems=["AC","+/-","%","÷","7","8","9","×","4","5","6","-","1","2","3","+","0",".","="];
+const numberItems = [
+  { title: "AC", isClick: false },
+  { title: "+/-", isClick: false },
+  { title: "%", isClick: false },
+  { title: "÷", isClick: false },
+  { title: "7", isClick: false },
+  { title: "8", isClick: false },
+  { title: "9", isClick: false },
+  { title: "×", isClick: false },
+  { title: "4", isClick: false },
+  { title: "5", isClick: false },
+  { title: "6", isClick: false },
+  { title: "-", isClick: false },
+  { title: "1", isClick: false },
+  { title: "2", isClick: false },
+  { title: "3", isClick: false },
+  { title: "+", isClick: false },
+  { title: "0", isClick: false },
+  { title: ".", isClick: false },
+  { title: "=", isClick: false },
+];
 
 function App() {
-  const [result,setResult] = useState(0);
-  const [pickTarget,setPickTarget] = useState([]);
-  const [isAfterAction,setIsAfterAction]=useState(false);
+  const [result, setResult] = useState(0);
+  const [pickTarget, setPickTarget] = useState([]);
+  const [isAfterAction, setIsAfterAction] = useState(false);
 
-
-  const findOperatorClass=(index)=>{
-    if(index===3 || index===7 || index===11 || index===15 ||index===18) return true;
+  const findOperatorClass = (index) => {
+    if (
+      index === 3 ||
+      index === 7 ||
+      index === 11 ||
+      index === 15 ||
+      index === 18
+    )
+      return true;
     return false;
   };
 
-
-  const toZero=()=>{
+  const toZero = () => {
     setResult(0);
     setPickTarget([]);
     setIsAfterAction(false);
   };
 
-  const togglePlusMinus = ()=>{
-    setResult((prev)=>{
-      if(prev>0){
+  const togglePlusMinus = () => {
+    setResult((prev) => {
+      if (prev > 0) {
         return -prev;
       }
       return Math.abs(prev);
-    })
+    });
   };
 
-  const getResult=()=>{
-    
-    const resultFun=pickTarget.join('').replace('×','*').replace("÷",'/');
-    ///^[\d\s()+\-*\/.]+$/ 
-    const answer=eval(resultFun);
+  const getResult = () => {
+    const resultFun = pickTarget.join("").replace("×", "*").replace("÷", "/");
+    ///^[\d\s()+\-*\/.]+$/
+    const answer = eval(resultFun);
     setResult(answer);
 
     setPickTarget([answer]);
     setIsAfterAction(false);
   };
 
-  const checkOnClickFunction=(e,isOperator)=>{
-    const value=e.target.innerText;
+  const checkOnClickFunction = (e, isOperator, index) => {
+    const value = e.target.innerText;
+    if (index && isOperator) {
+    }
 
-   if(isOperator && value!=="="){
-    setPickTarget((prev)=>([...prev,value]));
-    setIsAfterAction(true);
-    return;
-   };
-   //處理非加減乘除的情形
-   if(value === 'AC'){
-    toZero();
-    return;
-   };
+    if (isOperator && value !== "=") {
+      setPickTarget((prev) => [...prev, value]);
+      setIsAfterAction(true);
+      return;
+    }
+    //處理非加減乘除的情形
+    if (value === "AC") {
+      toZero();
+      return;
+    }
 
-   if(value === "+/-"){
-    togglePlusMinus();
-    return;
-   };
+    if (value === "+/-") {
+      togglePlusMinus();
+      return;
+    }
 
-   if(value === "%"){
-    setResult((prev)=>prev/100);
-    return;
-   };
+    if (value === "%") {
+      setResult((prev) => prev / 100);
+      return;
+    }
 
-   if(value === "." && !pickTarget.includes('.')){
-    setResult((prev)=>`${prev}.`);
-    setPickTarget((prev)=>([...prev,value]));
-    return;
-   }
+    if (value === "." && !pickTarget.includes(".")) {
+      setResult((prev) => `${prev}.`);
+      setPickTarget((prev) => [...prev, value]);
+      return;
+    }
 
-   if(value === "." && pickTarget.includes('.')){
-    return;
-   }
+    if (value === "." && pickTarget.includes(".")) {
+      return;
+    }
 
-   if(value === "="){
-    getResult();
-    return;
-   }
+    if (value === "=") {
+      getResult();
+      return;
+    }
 
-   //處理數字
+    //處理數字
 
-   if(result === 0){
-    setResult(value);
-    setPickTarget(value);
-    setIsAfterAction(false);
-    return;
-   };
+    if (result === 0) {
+      setResult(value);
+      setPickTarget(value);
+      setIsAfterAction(false);
+      return;
+    }
 
-   if(result!== 0){
-    isAfterAction? setResult(value): setResult((prev)=>`${prev}${value}`);
-    setPickTarget((prev)=>([...prev,value]));
-    setIsAfterAction(false);
+    if (result !== 0) {
+      isAfterAction ? setResult(value) : setResult((prev) => `${prev}${value}`);
+      setPickTarget((prev) => [...prev, value]);
+      setIsAfterAction(false);
 
-    return;
-   }
-   
+      return;
+    }
   };
-
- 
-
 
   return (
     <div className={styles.app}>
       <div className={styles.result}>
-      <Result result={result}/>
+        <Result result={result} />
       </div>
-    <div className={styles.numberArea}>
-      {numberItems.map((number,index)=>{
-        return(
-          <NumberItem key={number} data={number} operator={findOperatorClass(index)} onClick={(e)=>checkOnClickFunction(e,findOperatorClass(index))}/>
-        )
-      })}
-    </div>
-    
+      <div className={styles.numberArea}>
+        {numberItems.map((number, index) => {
+          return (
+            <NumberItem
+              // style={{ backgroundColor: "green" }}
+              key={number.title}
+              data={number.title}
+              operator={findOperatorClass(index)}
+              onClick={(e) =>
+                checkOnClickFunction(e, findOperatorClass(index), index)
+              }
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
--- src/components/NumberItem/NumberItem.js
+++ src/components/NumberItem/NumberItem.js
@@ -1,12 +1,15 @@
-import styles from './NumberItem.module.scss';
+import styles from "./NumberItem.module.scss";
 
-const NumberItem = ({data,operator,onClick}) => {
-  
-    return (
-        <div className={`${operator? styles.operator:styles.number}`} onClick={(e)=>onClick(e,operator)}> 
-            {data}
-        </div>
-     );
-}
- 
+const NumberItem = ({ data, operator, onClick, style }) => {
+  return (
+    <div
+      className={`${operator ? styles.operator : styles.number}`}
+      onClick={(e) => onClick(e, operator)}
+      style={style}
+    >
+      {data}
+    </div>
+  );
+};
+
 export default NumberItem;
