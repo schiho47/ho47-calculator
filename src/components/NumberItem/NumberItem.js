b8db55d07b24a229533648183a4852ae9de85d6d
schiho47 <84003532+schiho47@users.noreply.github.com>
2024-03-10 17:53:35 +0800

    2024-03-10增加加減乘除被點擊畫面效果

--- src/components/NumberItem/NumberItem.js
+++ src/components/NumberItem/NumberItem.js
@@ -1,12 +1,19 @@
 import styles from './NumberItem.module.scss';
+import { useState } from 'react';
 
-const NumberItem = ({data,operator,onClick}) => {
-  
-    return (
-        <div className={`${operator? styles.operator:styles.number}`} onClick={(e)=>onClick(e,operator)}> 
-            {data}
-        </div>
-     );
-}
- 
+const NumberItem = ({ data, operator, onClick, isSelectedOperator }) => {
+  const itemClassName =
+    operator && data !== isSelectedOperator
+      ? styles.operator
+      : operator && data === isSelectedOperator
+      ? styles.operatorSelected
+      : styles.number;
+
+  return (
+    <div className={`${styles.commonItem} ${itemClassName}`} onClick={onClick}>
+      {data}
+    </div>
+  );
+};
+
 export default NumberItem;
